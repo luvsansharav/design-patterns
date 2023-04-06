fe13db44feee69ba9ecd6b074c2f5fc7f8f9b73a
Loya Altangerel <luvsansharav.a@gmail.com>
2023-04-06 10:16:51 -0700

    fix conflict issue

--- src/main/java/factory_method/VisaReader.java
+++ src/main/java/factory_method/VisaReader.java
@@ -1,2 +1,8 @@
-package factory_method;public class VisaReader {
+package factory_method;
+
+public class VisaReader extends Reader {
+    @Override
+    public ICard readCard() {
+        return new VisaCard();
+    }
 }
